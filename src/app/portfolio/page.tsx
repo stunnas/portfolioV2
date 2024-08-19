5d43fb238baa1393c888c6038323f3add3b2a42b
Chase Albritton <59696546+stunnas@users.noreply.github.com>
2024-08-19 01:32:39 -0400

    project data change

--- src/app/portfolio/page.tsx
+++ src/app/portfolio/page.tsx
@@ -1,48 +1,10 @@
+import { projects } from '@/lib/data/projects';
 import { AspectRatio } from '@/components/neobrutalism-ui/aspect-ratio';
 
-export default function Page() {
-	const PROJECTS = [
-		{
-			name: 'Hello World',
-			description: 'An animated experience to the famous quote.',
-			liveLink: 'https://hello-world-stunna.vercel.app/',
-			previewImage: '/projects/helloworld.png',
-			repoUrl: 'https://github.com/stunnas/hello-world',
-		},
-		{
-			name: 'Compleo',
-			description: 'Landing page for AI powered productivity app.',
-			liveLink: 'https://compleo.vercel.app',
-			previewImage: '/projects/compleo.png',
-			repoUrl: 'https://github.com/stunnas/compleoWeb',
-		},
-		{
-			name: 'Moosie Couture',
-			description: 'All your animal drinkware and sticker needs!',
-			liveLink: 'https://moosie-couture.vercel.app',
-			previewImage: '/projects/moosiecouture.webp',
-			repoUrl: 'https://github.com/stunnas/moosie-couture',
-		},
-		{
-			name: 'Audio Glow',
-			description:
-				'Showcasing a music visualizer using ThreeJS alongside a playlist system.',
-			liveLink: 'https://audioglow.vercel.app/',
-			previewImage: '/projects/musicvisualizer.png',
-			repoUrl: 'https://github.com/stunnas/audioglow',
-		},
-		{
-			name: 'Word Assistant',
-			description: 'Check your text stats with the word assistant.',
-			liveLink: 'https://wordassistant.vercel.app/',
-			previewImage: '/projects/wordassistant.png',
-			repoUrl: 'https://github.com/stunnas/wordassistant',
-		},
-	];
-
+export default function PortfolioPage() {
 	return (
 		<>
-			{PROJECTS.map((project, id) => {
+			{projects.map((project, id) => {
 				return (
 					<div
 						className='dark:bg-darkBg border-b-4 border-r-4 border-b-black border-r-black bg-bg p-8 py-10 w600:px-[30px] w400:px-5'
